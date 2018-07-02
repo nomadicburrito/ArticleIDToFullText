52c328578b6ac5f820af9be31ceb8dd14e62af24
Chris Williams <c.williams094@gmail.com>
2018-07-02 15:50:47 -0400

    added rate limiting for doi search since Springer is rate limited

--- main.py
+++ main.py
@@ -1,19 +1,66 @@
-import sys, requests
+import sys, requests, time
+from collections import deque
 from requestSciDir import requestSciDir
 from parseIDs import parseIDs
+from requestSpringer import requestSpringer
 
 IDFile=sys.argv[1]
 
 parser=parseIDs(IDFile)
-PMIDList=parser.parse()
+IDList=parser.parse()
 
-for x in PMIDList:
-	print x
-	sciDir=requestSciDir(sys.argv[2], x)
+idType=sys.argv[2]
 
-	sciDir.request() #Request the article from science direct
-
-	if sciDir.r.status_code == requests.codes.ok:	#only write to file if our request is handled well
+def makeRequest():
+	sciDir=requestSciDir(sys.argv[3], x)
+	sciDir.requestDOI() #Request the article from science direct
+	if sciDir.r.status_code == requests.codes.ok:   #only write to file if our request is handled well
 		sciDir.write()
+	elif sciDir.r.status_code !=requests.codes.ok:
+		spring=requestSpringer(x)
+		spring.request()
+		spring.write()
 
-	print sciDir.r.status_code
+if idType=='pmid':
+	for x in IDList:
+		print x
+		sciDir=requestSciDir(sys.argv[3], x)
+
+		sciDir.requestPMID() #Request the article from science direct
+
+		if sciDir.r.status_code == requests.codes.ok:	#only write to file if our request is handled well
+			sciDir.write()
+
+		print sciDir.r.status_code
+
+elif idType=='doi':
+	
+	queue = deque(maxlen=5)	#springer has a rate limit, so it will be implemented for all requests
+	timer=time.time()
+	for x in IDList:
+		if not queue:
+			queue.append(time.time())
+			makeRequest()
+		elif len(queue)==queue.maxlen:
+			while((time.time()-queue[0])<1):
+				time.sleep(1-(time.time()-queue[0]))
+			queue.popleft()
+			queue.append(time.time())
+			makeRequest()
+		else:
+			while((time.time()-queue[0])<1):
+				time.sleep(1-(time.time()-queue[0]))
+			queue.append(time.time())
+			makeRequest()
+	print 'Runtime: '+str(time.time()-timer)
+#		  sciDir=requestSciDir(sys.argv[3], x)
+#		  sciDir.requestDOI() #Request the article from science direct
+#		  if sciDir.r.status_code == requests.codes.ok:   #only write to file if our request is handled well
+#			   sciDir.write()
+#		elif sciDir.r.status_code !=requests.codes.ok:
+#			spring=requestSpringer(x)
+#			spring.request()
+#			spring.write()
+#		  
+else:
+	print "Please enter a valid ID Type"
--- requestSciDir.py
+++ requestSciDir.py
@@ -2,15 +2,19 @@ import requests
 
 class requestSciDir:
 
-	def __init__(self, token, pmid):
+	def __init__(self, token, id):
 		self.token=token	#APIKey for science direct
-		self.pmid=pmid
+		self.id=id
 		self.headers={'Accept':'text/xml', 'X-ELS-APIKEY':self.token}	#Ask for XML and provide api key
 		self.payload={'view':'FULL'}	#Ask for all article information, including full article text
 
-	def request(self):	#perform html request
-		self.r = requests.get('https://api.elsevier.com/content/article/pubmed_id/'+self.pmid, params=self.payload, headers=self.headers)
+	def requestPMID(self):	#perform html request
+		self.r = requests.get('https://api.elsevier.com/content/article/pubmed_id/'+self.id, params=self.payload, headers=self.headers)
+
+	def requestDOI(self):
+		self.r = requests.get('https://api.elsevier.com/content/article/doi/'+self.id, params=self.payload, headers=self.headers)
+
 
 	def write(self):
-		with open(self.pmid+'.xml', 'w+') as file:
-			file.write(self.r.text.encode('utf-8'))
+		with open(self.id.replace('/', '_')+'.xml', 'w+') as file:
+			file.write(self.r.content)
